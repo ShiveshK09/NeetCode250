66a1570dc5d6e2e0374746b8e1b857b491752d97
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-09-13 03:54:58 +0530

    Add containsDuplicate method in Solution class
    Implement containsDuplicate method to check for duplicates in an array.

---
+++ Arrays & Hashing/2. Contains Duplicate.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/contains-duplicate/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int n:nums) if(!set.add(n)) return true;
+        return false;
+    }
+}
