dee945f12e52dc603c28e5610637ae99ffa150f0
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-09-13 04:05:28 +0530

    Add Two Sum solution in Java
    Implement twoSum method to find indices of two numbers that add up to a target.

---
+++ Arrays & Hashing/4. Two Sum.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/two-sum/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int val=target-nums[i];
+            if(map.containsKey(val)) return new int[]{i,map.get(val)};
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
