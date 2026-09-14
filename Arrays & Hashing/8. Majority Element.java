c29e72ab6e7ccc403e2c61fd7587dc2e53fd30bc
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-09-15 02:21:32 +0530

    Add majorityElement function to find majority element
    Implement the Boyer-Moore Voting Algorithm to find the majority element in an array.

---
+++ Arrays & Hashing/8. Majority Element.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/majority-element/
+class Solution {
+    public int majorityElement(int[] nums) {
+       int count=0,ele=0;
+       for(int n:nums){
+        if(count==0) ele=n;
+        if(n==ele) count++;
+        else count--;
+       } 
+       return ele;
+    }
+}
